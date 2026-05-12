0db8f4f60581c3fd4714b779ec5c5655c2b3ede9
giangduong1209 <giangduong120900@gmail.com>
2026-05-12 16:45:59 +0700

    chore: clean code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HR Management System",
-  description: "Hệ thống Quản lý Thông tin Nhân viên chuyên nghiệp",
+  description: "Hệ thống Quản lý Thông tin Nhân viên",
 };
 
 export default function RootLayout({
